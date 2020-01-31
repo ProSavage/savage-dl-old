dec1043808a669a6b23b8f7a1be62ff2850f8d1b
prosavage <naman0311@gmail.com>
2020-01-31 03:19:34 -0600

    move cors.

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./app/config/db');
 const Discord = require('discord.js');
-
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-
-
+app.use(cors(), bodyParser.urlencoded({extended: true}));
 
 const port = 2001;
-
-
-
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err);
     const client = new Discord.Client();
@@ -22,12 +17,6 @@ MongoClient.connect(db.url, (err, database) => {
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
     });
-    const cors = require('cors');
-    app.use(function (req, res, next) {
-        app.use(cors());
-        next();
-    });
-
     require('./app/routes')(app, database.db("savage-dl"), client);
     app.listen(port, () => {
         console.log("We are live on " + port);
