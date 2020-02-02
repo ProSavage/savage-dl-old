261c87de990d0d875c41b5dbdb52d3311e9ef7f6
prosavage <naman0311@gmail.com>
2020-02-02 00:16:28 -0600

    fix url

--- app/routes/dl_routes.js
+++ app/routes/dl_routes.js
@@ -49,9 +49,8 @@ module.exports = function (app, db, client) {
             })
     });
 
-    app.get(`auth/name/:code`, (req, res) => {
+    app.get(`/auth/name/:code`, (req, res) => {
         console.log("Getting user information.");
-        res.send({reeeee: "re"});
         const session = req.params.code;
         const token = sessions[session];
         if (!token) {
